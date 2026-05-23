6ca6c1420e4c5bc516876d12bb4cf81aaedc0709
vunguyensytuong <vunguyensytuong@gmail.com>
2026-05-23 11:02:21 +0700

    Create main6.cpp

---
+++ main6.cpp
@@ -0,0 +1,8 @@
+#include "XuLy.h"
+int main() {
+    // tao file input de test yeu cau kiem tra dieu kien
+    taoFileDauVao();
+    // bat dau chay thuat toan chinh
+    xuLyVaXuatFile();
+    return 0;
+}
